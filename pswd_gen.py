81c87ef7b4cce7c52386c4a6b0cc67a49924485f
Father Misail <211843152+FatherMisail@users.noreply.github.com>
2025-05-18 12:07:51 +0700

    Генератор паролей отрефакторен

--- pswd_gen.py
+++ pswd_gen.py
@@ -1,4 +1,10 @@
-import random, math
+import random
+
+digits = "0123456789"
+lowercase_letters = "abcdefghijklmnopqrstuvwxyz"
+uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+punctuation = "!#$%&*+-=?@^_."
+ambiguous_sym = 'il1Lo0O'
 
 
 def is_nat(n):
@@ -7,10 +13,8 @@ def is_nat(n):
 
 def generate_password(length, chars):
     pswd = []
-    new_pswd_len = length + math.floor(length ** (1 / 3) * random.random() * 3)
-    al = []
-    al.extend(chars)
-    random.shuffle(al)
+    al = list(chars)
+    new_pswd_len = length + random.randint(0,5)
 
     for _ in range(new_pswd_len):
         pswd.append(random.choice(al))
@@ -29,22 +33,21 @@ def generate_passwords(pswd_cnt, pswd_len, chars):
 
 
 def main():
-    digits = "0123456789"
-    lowercase_letters = "abcdefghijklmnopqrstuvwxyz"
-    uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    punctuation = "!#$%&*+-=?@^_."
-    ambiguous_sym = 'il1Lo0O'
     chars = ''
 
     while True:
-        print('Сколько паролей сгенерировать?')
+        print('Введите количество генерируемых паролей: ', end='')
         pswd_cnt = input()
         if is_nat(pswd_cnt):
             pswd_cnt = int(pswd_cnt)
             break
 
+    if pswd_cnt == 0:
+        print('Нечего генерировать')
+        return
+
     while True:
-        print('Какова примерная длинна пароля?')
+        print('Введите примерную длинну пароля: ', end='')
         pswd_len = input()
         if is_nat(pswd_len):
             pswd_len = int(pswd_len)
@@ -54,49 +57,55 @@ def main():
                 print('Длинна пароля не должна быть меньше 8 символов')
 
     while True:
-        print('Включать ли цифры <0123456789>? Введите <Д> или <Н>:)')
-        ans = input()
-        if ans == 'Д':
-            chars += digits
-            break
-        elif ans == 'Н':
-            break
+        while True:
+            print('Включать ли цифры <0123456789>? Введите <Д> или <Н>: ', end='')
+            ans = input()
+            if ans == 'Д':
+                chars += digits
+                break
+            elif ans == 'Н':
+                break
 
-    while True:
-        print('Включать ли прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ? Введите <Д> или <Н>:)')
-        ans = input()
-        if ans == 'Д':
-            chars += uppercase_letters
-            break
-        elif ans == 'Н':
-            break
+        while True:
+            print('Включать ли прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ? Введите <Д> или <Н>: ', end='')
+            ans = input()
+            if ans == 'Д':
+                chars += uppercase_letters
+                break
+            elif ans == 'Н':
+                break
 
-    while True:
-        print('Включать ли строчные буквы abcdefghijklmnopqrstuvwxyz? Введите <Д> или <Н>:)')
-        ans = input()
-        if ans == 'Д':
-            chars += lowercase_letters
-            break
-        elif ans == 'Н':
-            break
+        while True:
+            print('Включать ли строчные буквы abcdefghijklmnopqrstuvwxyz? Введите <Д> или <Н>: ', end='')
+            ans = input()
+            if ans == 'Д':
+                chars += lowercase_letters
+                break
+            elif ans == 'Н':
+                break
 
-    while True:
-        print('Включать ли символы !#$%&*+-=?@^_? Введите <Д> или <Н>:)')
-        ans = input()
-        if ans == 'Д':
-            chars += punctuation
-            break
-        elif ans == 'Н':
-            break
+        while True:
+            print('Включать ли символы !#$%&*+-=?@^_? Введите <Д> или <Н>: ', end='')
+            ans = input()
+            if ans == 'Д':
+                chars += punctuation
+                break
+            elif ans == 'Н':
+                break
 
-    while True:
-        print('Исключать ли неоднозначные символы il1Lo0O? Введите <Д> или <Н>:)')
-        ans = input()
-        if ans == 'Д':
-            for c in ambiguous_sym:
-                chars = chars.replace(c, '')
-            break
-        elif ans == 'Н':
+        while True:
+            print('Исключать ли неоднозначные символы il1Lo0O? Введите <Д> или <Н>: ', end='')
+            ans = input()
+            if ans == 'Д':
+                for c in ambiguous_sym:
+                    chars = chars.replace(c, '')
+                break
+            elif ans == 'Н':
+                break
+
+        if not chars:
+            print('Надо выбрать из каких символов составлять пароль!')
+        else:
             break
 
     print(*generate_passwords(pswd_cnt, pswd_len, chars), sep='\n')
