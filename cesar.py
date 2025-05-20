ecb0817944eab3d11957bf0178ff5bc60be46863
Father Misail <211843152+FatherMisail@users.noreply.github.com>
2025-05-20 22:57:32 +0700

    Добавлена возможность подбора ключа для шифровки

--- cesar.py
+++ cesar.py
@@ -1,12 +1,18 @@
+RU_MOD = ord('Я') - ord('А') + 1
+EN_MOD = ord('Z') - ord('A') + 1
+
+
+def get_mod(lang):
+    if lang == 'ru':
+        return RU_MOD
+    elif lang == 'en':
+        return EN_MOD
+
+
 def crypt(lang, k, msg):
     cipher = []
 
-    n = 0
-    if lang == 'ru':
-        n = ord('Я') - ord('А') + 1
-    elif lang == 'en':
-        n = ord('Z') - ord('A') + 1
-
+    n = get_mod(lang)
     for c in msg:
         base = -1
         if lang == 'ru':
@@ -34,21 +40,37 @@ def decrypt(lang, k, msg):
     return crypt(lang, -k, msg)
 
 
+def string_crack(lang, msg):
+    n = get_mod(lang)
+    for i in range(1, n):
+        print(f'Ключ <{i}>: ', crypt(lang, i, msg))
+
+
 def main():
     while True:
-        print('Введите <0> для шифровки')
-        print('Введите <1> для дешифровки: ', end='')
+        print('Введите <0> для зашифровки по ключу')
+        print('Введите <1> для дешифровки по ключу')
+        print('Введите <2> для дешифровки перебором: ', end='')
         ct_dct = input()
-        if '0' == ct_dct or ct_dct == '1':
+        if '0' == ct_dct or ct_dct == '1' or ct_dct == '2':
             break
     ct_dct = int(ct_dct)
 
     while True:
-        print('Введите <ru> для шифрования символов русского языка текста или <en> для символов английского языка.')
-        print('Иные символы шифроваться не будут: ', end='')
+        print('Введите <ru> для обработки символов русского языка или <en> для английского.')
+        print('Иные символы затронуты не будут: ', end='')
         lang = input()
         if 'ru' == lang or lang == 'en':
             break
+
+    msg = ''
+    while not msg:
+        print('Введите сообщение: ', end='')
+        msg = input()
+
+    if ct_dct == 2:
+        string_crack(lang, msg)
+        return
 
     while True:
         print('Введите целое неотрицательное число-ключ: ', end='')
@@ -56,11 +78,6 @@ def main():
         if k.isdigit():
             break
     k = int(k)
-
-    msg = ''
-    while not msg:
-        print('Введите сообщение: ', end='')
-        msg = input()
 
     if ct_dct == 0:
         res = crypt(lang, k, msg)
