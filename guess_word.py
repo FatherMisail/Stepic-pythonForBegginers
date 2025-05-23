d41a54b0cae07f1aedd4492c9de7212ca54534b5
Father Misail <211843152+FatherMisail@users.noreply.github.com>
2025-05-23 09:41:33 +0700

    Рефакторинг угадайки завершен

--- guess_word.py
+++ guess_word.py
@@ -88,7 +88,7 @@ def input_is_valid(s):
         return False
 
     for c in s:
-        if 'А' <= c <= 'Я':
+        if 'А' <= c <= 'Я' or c == 'Ё':
             continue
         return False
     return True
@@ -104,11 +104,11 @@ def find_all(c, s):
     return positions
 
 
-def replace_letters(s, word_completion, word):
+def replace_letter(c, word_completion, word):
     wc = list(word_completion)
-    for c in s:
-        for i in find_all(c, word):
-            wc[i] = c
+
+    for i in find_all(c, word):
+        wc[i] = c
 
     return ''.join(wc)
 
@@ -118,30 +118,31 @@ def play(word):
     guessed = False  # сигнальная метка
     guessed_letters = []  # список уже названных букв -
     # зачем называть названные буквы - угаданными - ума не
-    # приложу, но так требует задание...
-    guessed_words = []  # список уже названных слов
+    # приложу, но так требует задание... так, что это - все введённые буквы (даже неугаданные)
+    guessed_words = []  # список уже названных слов - все введённые слова (даже неугаданные)
     tries = 6
 
     print('Давайте играть в угадайку слов!')
     print(display_hangman(tries))
-    print(word_completion)
 
     while not guessed and tries:
+        print(word_completion)
+
         s = input().upper()
         if input_is_valid(s):
             if len(s) == 1:
                 if s[0] in guessed_letters:
                     print('Эта буква уже использована')
-                    print(word_completion)
+                    # print(word_completion)
                     continue
                 else:
                     guessed_letters.append(s[0])
 
-                word_completion_new = replace_letters(s[0], word_completion, word)
+                word_completion_new = replace_letter(s[0], word_completion, word)
             else:
                 if s in guessed_words:
                     print('Это слово уже использовано')
-                    print(word_completion)
+                    # print(word_completion)
                     continue
                 else:
                     guessed_words.append(s)
@@ -157,14 +158,11 @@ def play(word):
             else:
                 word_completion = word_completion_new
 
-            print(word_completion)
-
             if '_' not in word_completion:
                 print('Поздравляем, вы угадали слово! Вы победили!')
                 guessed = True
         else:
             print('Вводите только буквы русского алфавита')
-            print(word_completion)
 
     if not guessed:
         print(word)
@@ -173,8 +171,8 @@ def play(word):
 def main():
     while True:
         play(get_word())
-        print('Еще раз?')
-        s = input()
+        print('Еще раз? Введите <Н> если хотите прерваться и что угодно, чтобы продолжить: ', end='')
+        s = input().upper()
         if s == 'Н':
             break
 
